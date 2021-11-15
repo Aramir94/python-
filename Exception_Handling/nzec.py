5aaed0dcb188ab6303af23a95ee84f9d85f33587
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2021-11-15 12:11:11 +0900

    non-zero exit code

---
+++ Exception_Handling/nzec.py
@@ -0,0 +1,12 @@
+try:
+    for n in range(int(input())):
+        a=int(int())
+        b=list(map(int,input().split()))
+        c=0
+        ls=Null
+        for i in b:
+            if n==Null or n>i:
+                n=i
+                c=c+1
+        print(c)
+except: pass
